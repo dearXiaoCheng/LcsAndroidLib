dcbb6430c71120606c8ade58bd84e067510f80ce
刘春爽 <liuchunshuang@bupt.edu.cn>
2021-03-16 22:13:51 +0800

    添加获取资源扩展

--- arch/src/main/java/com/lcs/arch/ext/ResourcesExt.kt
+++ arch/src/main/java/com/lcs/arch/ext/ResourcesExt.kt
@@ -1,6 +1,11 @@
 package com.lcs.arch.ext
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
 
 /**
  * @ClassName: ResourcesExt
@@ -9,7 +14,22 @@ import android.content.Context
  * @Description: ResourcesExt
  */
 
+/**
+ * 获取颜色
+ * @param color 颜色资源[ColorRes]
+ * @return [ColorInt]
+ */
+@ColorInt
+fun Context.colorOf(@ColorRes color: Int): Int {
+    return ContextCompat.getColor(this, color)
+}
 
-fun Context.colorOf() {
 
+/**
+ * 获取Drawable
+ * @param drawable 图片资源[ColorRes]
+ * @return 图片Drawable，可能为空
+ */
+fun Context.drawableOf(@DrawableRes drawable: Int): Drawable? {
+    return ContextCompat.getDrawable(this, drawable)
 }
