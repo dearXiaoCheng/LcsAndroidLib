be2e2d3f1c87b3cd5b732ca6303e222bd9ee5c53
刘春爽 <liuchunshuang@bupt.edu.cn>
2021-03-26 23:05:32 +0800

    移除控制View的显隐代码，ktx中已有

--- arch/src/main/java/com/lcs/arch/ext/ViewExt.kt
+++ arch/src/main/java/com/lcs/arch/ext/ViewExt.kt
@@ -1,6 +1,5 @@
 package com.lcs.arch.ext
 
-import android.view.View
 import android.widget.EditText
 
 /**
@@ -10,26 +9,6 @@ import android.widget.EditText
  * @Date: 2021/3/16 下午10:00
  */
 
-/**
- * 显示View：设置为[View.VISIBLE]
- */
-fun View.show() {
-    this.visibility = View.VISIBLE
-}
-
-/**
- * 使View消失：设置为[View.GONE]
- */
-fun View.gone() {
-    this.visibility = View.GONE
-}
-
-/**
- * 隐藏View：设置为[View.INVISIBLE]
- */
-fun View.hide() {
-    this.visibility = View.INVISIBLE
-}
 
 /**
  * 返回[EditText]的[EditText.getText]，并转为String
